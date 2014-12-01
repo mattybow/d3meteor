c22bbcdbf441325a61cd1ef29419698e45b9c22b
mattybow <mattybow@gmail.com>
2014-12-01 15:23:30 -0600

    footer added

--- client/templates/application/navbar.js
+++ client/templates/application/navbar.js
@@ -1,5 +1,13 @@
+Template.navbar.rendered=function(){
+	console.log('navbar rendered');
+};
+
 Template.navbar.events({
-	'click':function(){
-		console.log('a click');
+	'click .navigation':function(e){
+		console.log('click on navbar');
+	},
+	'click a':function(e){
+		//e.stopPropagation();
+		console.log('click on a tag');
 	}
 });
